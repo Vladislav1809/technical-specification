01082aa5019171c19506cff9126e92d58e7da946
Vladislav <lik_200@mail.ru>
2022-03-02 01:53:32 +0300

    create config.php

---
+++ config.php
@@ -0,0 +1,47 @@
+<?php
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Cache\ArrayCache;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
+use Doctrine\ORM\Tools\Setup;
+
+require_once "vendor/autoload.php";
+
+/**
+ * @return EntityManager
+ * @throws \Doctrine\DBAL\DBALException
+ * @throws \Doctrine\ORM\ORMException
+ */
+function GetEntityManager()
+{
+    $config = Setup::createConfiguration(true, __DIR__ . '/var/cache', new ArrayCache());
+    $driver = new AnnotationDriver(new AnnotationReader(), [__DIR__ . '/entity']);
+    $dbParams = array(
+        'driver' => 'pdo_pgsql',
+        'user' => 'vladislav',
+        'password' => '7879031318',
+        'dbname' => 'test',
+    );
+    $connectionParams = array(
+        'dbname' => 'test',
+        'user' => 'vladislav',
+        'password' => '7879031318',
+        'host' => 'localhost',
+        'driver' => 'pdo_pgsql',
+    );
+    $config->setMetadataDriverImpl($driver);
+    $config->setProxyDir(__DIR__ . '/var/cache');
+    $entityManager = EntityManager::create($connectionParams, $config);
+
+    return $entityManager;
+}
+
+$entityManager = GetEntityManager();
+
+$sql = "SELECT * FROM customers";
+
+$result = $entityManager->getConnection()->executeQuery($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+var_dump(
+    $result
+);
