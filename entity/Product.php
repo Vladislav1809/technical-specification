d712975e12f902380a0ab74964c080163150215e
Vladislav <lik_200@mail.ru>
2022-03-02 03:08:57 +0300

    next update Product.php

--- entity/Product.php
+++ entity/Product.php
@@ -1,14 +1,14 @@
 <?php
  // entity/Product.php
+/**
+ * @Entity @Table(name="products")
+**/
 class Product
 {
-    /**
-     * @var int
-     */
+    /** @Id @Column(type="integer") @GeneratedValue **/
     protected $id;
-    /**
-     * @var string
-     */
+
+    /** @Column(type="string") **/
     protected $name;
 
     public function getId()
