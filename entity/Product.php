aa3c57ece86d93447fbe53e17d1048a3a2f69ab0
Vladislav <lik_200@mail.ru>
2022-03-02 03:00:12 +0300

    update Product.php

--- entity/Product.php
+++ entity/Product.php
@@ -1,2 +1,29 @@
 <?php
+ // entity/Product.php
+class Product
+{
+    /**
+     * @var int
+     */
+    protected $id;
+    /**
+     * @var string
+     */
+    protected $name;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+}
 
